b7ab91e4082de7e5f33cf3fe9a1ebc4cb2c59de1
Zavire Shiran <zavire.shiran@gmail.com>
2015-06-26 16:50:05 -0500

    Add eqv tests for numbers.

--- interpreter/tests/test.cpp
+++ interpreter/tests/test.cpp
@@ -32,3 +32,23 @@ TEST(Testing, NilNumEqTest) {
     ASSERT_FALSE(eq(n, num));
     ASSERT_FALSE(eq(num, n));
 }
+
+TEST(Testing, NumEqvTest) {
+    shared_ptr<lispobj> zero(new number(0));
+    shared_ptr<lispobj> zero2(new number(0));
+    shared_ptr<lispobj> one(new number(1));
+    shared_ptr<lispobj> one2(new number(1));
+
+    ASSERT_TRUE(eqv(zero, zero));
+    ASSERT_TRUE(eqv(zero2, zero2));
+    ASSERT_TRUE(eqv(zero, zero2));
+    ASSERT_TRUE(eqv(zero2, zero));
+
+    ASSERT_TRUE(eqv(one, one));
+    ASSERT_TRUE(eqv(one2, one2));
+    ASSERT_TRUE(eqv(one, one2));
+    ASSERT_TRUE(eqv(one2, one));
+
+    ASSERT_FALSE(eqv(zero, one));
+    ASSERT_FALSE(eqv(one, zero));
+}
