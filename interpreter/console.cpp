707dfde770209d6cb0d85715c822f465f6523ff6
Zavire Shiran <zavire.shiran@gmail.com>
2017-05-06 01:38:47 -0500

    some final console module updates

--- interpreter/console.cpp
+++ interpreter/console.cpp
@@ -12,6 +12,11 @@ shared_ptr<lispobj> console_init(vector< shared_ptr<lispobj> > /*args*/) {
     printw("Hello World !!!");
     refresh();
     getch();
+
+    return make_shared<nil>();
+}
+
+shared_ptr<lispobj> console_end(vector< shared_ptr<lispobj> > /*args*/) {
     endwin();
 
     return make_shared<nil>();
@@ -23,6 +28,7 @@ shared_ptr<module> make_console_module(shared_ptr<lexicalscope> top_level_scope)
     shared_ptr<module> console_module(new module(module_name, top_level_scope));
 
     console_module->defun_and_export("console-init", make_shared<cfunc>(console_init));
+    console_module->defun_and_export("console-end", make_shared<cfunc>(console_end));
 
     return console_module;
 }
